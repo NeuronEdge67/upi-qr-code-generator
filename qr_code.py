81acab6ffa1b9a9ed7a165d8d84cd0d04ddb4096
akhilesh2009 <akhileshakhilesh2880@gmail.com>
2026-09-18 15:53:17 +0530

    removed tn(transaction note

--- qr_code.py
+++ qr_code.py
@@ -2,7 +2,7 @@ import qrcode
 
 upi_id = input("Enter your UPI ID: ")
 amount = float(input("Enter Amount: "))
-upi_url = f"upi://pay?pa={upi_id}&am={amount}&cu=INR&tn=Tea"
+upi_url = f"upi://pay?pa={upi_id}&am={amount}&cu=INR&"
 
 qr = qrcode.QRCode()
 qr.add_data(upi_url)
