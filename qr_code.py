8e1504280a8e81a548ff6925e5e72172b0a7a475
akhilesh2009 <akhileshakhilesh2880@gmail.com>
2026-09-18 15:49:28 +0530

    instead of saving the png now it directly print it in the terminal

--- qr_code.py
+++ qr_code.py
@@ -4,6 +4,7 @@ upi_id = input("Enter your UPI ID: ")
 amount = float(input("Enter Amount: "))
 upi_url = f"upi://pay?pa={upi_id}&am={amount}&cu=INR&tn=Tea"
 
-img = qrcode.make(upi_url)
-
-img.save(f"qr_{upi_id}_amount_{amount}.png")
+qr = qrcode.QRCode()
+qr.add_data(upi_url)
+qr.make(fit=True)
+qr.print_ascii(invert=True)
